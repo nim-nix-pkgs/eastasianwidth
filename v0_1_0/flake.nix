958800a969928ed12a774e3382e60bfa66bc2060
hugosenari <hugosenari@gmail.com>
2022-03-27 21:49:14 -0300

    chore: re index eastasianwidth

--- v0_1_0/flake.nix
+++ v0_1_0/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-eastasianwidth-v0_1_0.ref   = "refs/tags/v0.1.0";
   inputs.src-eastasianwidth-v0_1_0.owner = "jiro4989";
   inputs.src-eastasianwidth-v0_1_0.repo  = "eastasianwidth";
-  inputs.src-eastasianwidth-v0_1_0.dir   = "";
   inputs.src-eastasianwidth-v0_1_0.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
